752a7adc894f8be2b4f9634376e71da72f04d3f5
Logan Dihel <logan.dihel@gmail.com>
2022-04-14 11:30:06 -0700

    Now running on epxress app

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const { createGameState, processGuess, checkWordIsCorrect,  updateCorrectWord, checkWinner} = require('./game');
 const { makeid } = require('./utils');
 
-const httpServer = require("http").createServer();
-const io = require("socket.io")(httpServer, {
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
     cors: {
-        origin: "http://127.0.0.1:8080",
-        methods: ["GET", "POST"]
+      origin: "http://127.0.0.1:8080"
     }
 });
-  
-io.listen(3000);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
 
 const state = {};
 const clientRooms = {};
